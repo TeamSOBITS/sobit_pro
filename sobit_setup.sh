15ee4a7b7005ed2367f46b8f4e586cdef7ff0163
Yuki-Ikeda0810 <yukiikeda2@gmail.com>
2021-10-21 10:57:28 +0000

    Update sobit_setup.sh

--- sobit_setup.sh
+++ sobit_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(pwd)/src
+cd $(pwd)/..
 
 # git cloneしたいTeamSOBITSのROSパッケージを記述
 # "azure_kinect_ros"
@@ -68,6 +68,34 @@ echo "# Bus 002 Device 116: ID 045e:097a Microsoft Corp.
       ATTRS{idVendor}==\"045e\", ATTRS{idProduct}==\"097e\", MODE=\"0666\", GROUP=\"plugdev\"
       LABEL=\"k4a_logic_rules_end\"" | sudo tee /etc/udev/rules.d/99-k4a.rules
 # sudo /etc/init.d/udev reload
+
+# Set up arduino-nano rules
+echo "ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"7523\", MODE=\"0666\", RUN+=\"/bin/stty -F /dev/ttyUSB0\"" | sudo tee /etc/udev/rules.d/60-arduino-nano.rules
+​# sudo /etc/init.d/udev reload
+
+# Set up kobuki rules
+echo "# On precise, for some reason, USER and GROUP are getting ignored.
+      # So setting mode = 0666 for now.
+      SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"kobuki*\", 
+      ATTR{device/latency_timer}=\"1\", MODE:=\"0666\", GROUP:=\"dialout\", SYMLINK+=\"input/kobuki\", KERNEL==\"ttyUSB*\"
+      # Bluetooth module (currently not supported and may have problems)
+      # SUBSYSTEM==\"tty\", ATTRS{address}==\"00:00:00:41:48:22\", MODE:=\"0666\", GROUP:=\"dialout\", SYMLINK+=\"input/kobuki\"" | sudo tee /etc/udev/rules.d/57-kobuki.rules
+#sudo /etc/init.d/udev reload
+​
+# Set up xtion rules
+echo "# Make primesense device mount with writing permissions (default is read only for unknown devices)
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0200\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0300\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0401\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0500\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0600\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0601\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"0609\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"1280\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"2100\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"2200\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"
+      SUBSYSTEM==\"usb\", ATTR{idProduct}==\"f9db\", ATTR{idVendor}==\"1d27\", MODE:=\"0666\", OWNER:=\"root\", GROUP:=\"video\"" | sudo tee /etc/udev/rules.d/55-primesense-usb.rules
+#sudo /etc/init.d/udev reload
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
