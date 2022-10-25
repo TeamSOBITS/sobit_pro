f18e9736e0ed858ebcff705e4b691adb1dbdea90
MrKeith99 <kvalentincardenas@hotmail.com>
2022-10-25 14:57:11 +0900

    update function names

--- sobit_pro_library/example/test_grasp_on_floor.cpp
+++ sobit_pro_library/example/test_grasp_on_floor.cpp
@@ -12,11 +12,11 @@ int main(int argc, char *argv[]) {
     std::string target_name = "potato_chips";
 
     // Pose: detecting_pose
-    pro_joint_ctrl.moveToRegisterdMotion("detecting_pose");
+    pro_joint_ctrl.moveToPose("detecting_pose");
     ros::Duration(3.0).sleep();
 
     // Open the hand
-    pro_joint_ctrl.moveJoint( sobit_pro::Joint::GRIPPER_JOINT, 1.57, 2.0, true );
+    pro_joint_ctrl.moveJoint( sobit_pro::Joint::HAND_JOINT, 1.57, 2.0, true );
     ros::Duration(2.0).sleep();
 
     // Move the hand towards the target "target_name" and check if grasped
@@ -28,15 +28,15 @@ int main(int argc, char *argv[]) {
 
     if (grasp_flag){
         // Close Hand
-        pro_joint_ctrl.moveJoint(sobit_pro::Joint::GRIPPER_JOINT, 0.0, 2.0, true);
+        pro_joint_ctrl.moveJoint(sobit_pro::Joint::HAND_JOINT, 0.0, 2.0, true);
 
         //Pose: grasp_high_pose
-        pro_joint_ctrl.moveToRegisterdMotion("grasp_high_pose");
+        pro_joint_ctrl.moveToPose("grasp_high_pose");
         ros::Duration(2.0).sleep();
     }
 
     // Pose: "initial pose"
-    pro_joint_ctrl.moveToRegisterdMotion("initial_pose");
+    pro_joint_ctrl.moveToPose("initial_pose");
     ros::Duration(2.0).sleep();
 
     return 0;
