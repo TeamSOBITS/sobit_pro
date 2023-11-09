76fa9657fa4f7d7313718a494952e3e551c26df7
MrKeith99 <kvalentincardenas@hotmail.com>
2023-11-09 14:33:55 +0900

    [UPD] indent from 2 to 4 space

--- sobit_pro_control/include/sobit_pro_control/sobit_pro_motor_driver.hpp
+++ sobit_pro_control/include/sobit_pro_control/sobit_pro_motor_driver.hpp
@@ -25,17 +25,17 @@
 #define PROTOCOL_VERSION                2.0
 
 // Default setting
-#define WHEEL_F_R                       1              // Dynamixel ID:1
 #define WHEEL_F_L                       2              // Dynamixel ID:2
-#define WHEEL_B_R                       3              // Dynamixel ID:3
+#define WHEEL_F_R                       1              // Dynamixel ID:1
 #define WHEEL_B_L                       4              // Dynamixel ID:4
+#define WHEEL_B_R                       3              // Dynamixel ID:3
 
-#define STEER_F_R                       5              // Dynamixel ID:5
 #define STEER_F_L                       6              // Dynamixel ID:6
-#define STEER_B_R                       7              // Dynamixel ID:7
+#define STEER_F_R                       5              // Dynamixel ID:5
 #define STEER_B_L                       8              // Dynamixel ID:8
+#define STEER_B_R                       7              // Dynamixel ID:7
 
-#define BAUDRATE                        3000000        // baud rate of Dynamixel
+#define BAUDRATE                        3000000        // Baudrate of Dynamixel
 #define DEVICENAME                      "/dev/input/dx_lower"
 
 #define TORQUE_ENABLE                   1              // Value for enabling the torque
@@ -43,45 +43,45 @@
 #define DXL_MOVING_STATUS_THRESHOLD     20             // Dynamixel moving status threshold  // old param : 10
 
 class SobitProMotorDriver{
-  public:
-    SobitProMotorDriver();
-    ~SobitProMotorDriver();
-    bool init(void);
-    void closeDynamixel(void);
-    bool setTorque(uint8_t id, bool onoff);
-    bool setProfileAcceleration(uint8_t id, uint32_t value);
-    bool setProfileVelocity(uint8_t id, uint32_t value);
-    bool controlSteers(int32_t *value);
-    bool controlWheels(int32_t *value);
-    bool addPresentParam(void);
-    int32_t feedbackSteer(uint8_t);
-    int32_t feedbackWheel(uint8_t);
+    public:
+        SobitProMotorDriver();
+        ~SobitProMotorDriver();
+        bool init(void);
+        void closeDynamixel(void);
+        bool setTorque(uint8_t id, bool onoff);
+        bool setProfileAcceleration(uint8_t id, uint32_t value);
+        bool setProfileVelocity(uint8_t id, uint32_t value);
+        bool controlSteers(int32_t *value);
+        bool controlWheels(int32_t *value);
+        bool addPresentParam(void);
+        int32_t feedbackSteer(uint8_t);
+        int32_t feedbackWheel(uint8_t);
 
-  private:
-    uint32_t baudrate_;
-    float  protocol_version_;
+    private:
+        uint32_t baudrate_;
+        float  protocol_version_;
 
-    // Initialize PortHandler instance
-    // Set the port path
-    // Get methods and members of PortHandlerLinux
-    dynamixel::PortHandler *portHandler_;
+        // Initialize PortHandler instance
+        // Set the port path
+        // Get methods and members of PortHandlerLinux
+        dynamixel::PortHandler *portHandler_;
 
-    // Initialize PacketHandler instance
-    // Set the protocol version
-    // Get methods and members of Protocol2PacketHandler
-    dynamixel::PacketHandler *packetHandler_;
+        // Initialize PacketHandler instance
+        // Set the protocol version
+        // Get methods and members of Protocol2PacketHandler
+        dynamixel::PacketHandler *packetHandler_;
 
-    // Initialize GroupSyncWrite instance
-    dynamixel::GroupSyncWrite *groupSyncWritePosition_;
+        // Initialize GroupSyncWrite instance
+        dynamixel::GroupSyncWrite *groupSyncWritePosition_;
 
-    // Initialize GroupSyncWrite instance
-    dynamixel::GroupSyncWrite *groupSyncWriteVelocity_;
+        // Initialize GroupSyncWrite instance
+        dynamixel::GroupSyncWrite *groupSyncWriteVelocity_;
 
-    // Initialize GroupSyncRead instance
-    dynamixel::GroupSyncRead *groupSyncReadPosition_;
+        // Initialize GroupSyncRead instance
+        dynamixel::GroupSyncRead *groupSyncReadPosition_;
 
-    // Initialize GroupSyncRead instance
-    dynamixel::GroupSyncRead *groupSyncReadVelocity_;
+        // Initialize GroupSyncRead instance
+        dynamixel::GroupSyncRead *groupSyncReadVelocity_;
 };
 
 #endif // SOBIT_PRO_MOTOR_DRIVER_H_
