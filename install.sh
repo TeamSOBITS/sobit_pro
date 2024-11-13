0011ab970d42ca2ee0b32361b71a729f783c58a4
OnoFumiya <101236167+OnoFumiya@users.noreply.github.com>
2024-11-13 16:14:10 +0900

    FIX install.sh to sobits_common from sobit_common

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd ..
 
 # Download required packages for SOBIT PRO
 ros_packages=(
-    "sobit_common" \
+    "sobits_common" \
     "sobits_msgs" \
     "urg_node" \
     "azure_kinect_ros_driver" \
