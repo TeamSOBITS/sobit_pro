3130b3f0cea6f896c8072e36c4efabf7581dd9de
Jiahao9 <simjiahao9@gmail.com>
2025-01-25 21:06:24 +0900

    [UPD] migration from ROS 1

---
+++ sobit_pro_description/launch/display.launch.py
@@ -0,0 +1,78 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    use_gui = LaunchConfiguration('use_gui', default='True')
+
+    robot_namespace = "sobit_pro"
+
+    rviz_config = os.path.join(get_package_share_directory(
+        'sobit_pro_description'), "rviz", "display.rviz")
+    
+    robot_description = os.path.join(get_package_share_directory(
+        'sobit_pro_description'), 
+        'robots',
+        'sobit_pro_robot.urdf.xacro'
+    )
+
+    xacro_arguments = {
+        'enable_gz': 'True',
+        'enable_mb': 'True',
+        'enable_arm': 'True',
+        'enable_head': 'True',
+        'robot_namespace': robot_namespace
+    }
+
+    robot_description_config = xacro.process_file(
+        robot_description,
+        mappings=xacro_arguments
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace=robot_namespace,
+        parameters=[{
+            "frame_prefix": robot_namespace + '/',
+            "robot_description": robot_description_config.toxml(),
+            "use_sim_time": True,
+        }],
+        output="screen",
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        output='screen',
+        namespace=robot_namespace,
+        condition=UnlessCondition(use_gui)
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        namespace=robot_namespace,
+        condition=IfCondition(use_gui)
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config],
+    )
+
+    return LaunchDescription([
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ])
