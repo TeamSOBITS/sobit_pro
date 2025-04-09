8327c6ff47ca4b8ddc2bb43e243366fb7e82eab9
MrKeith99 <kvalentincardenas@hotmail.com>
2025-04-09 22:17:20 +0900

    [FIX] indent, init into cpp

--- sobit_pro_control/include/sobit_pro_control/sobit_pro_main.hpp
+++ sobit_pro_control/include/sobit_pro_control/sobit_pro_main.hpp
@@ -4,115 +4,128 @@
 #include <iostream>
 #include <random>
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "sensor_msgs/msg/joint_state.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "trajectory_msgs/msg/joint_trajectory.hpp"
-#include "std_msgs/msg/float64_multi_array.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 #include "control_msgs/action/follow_joint_trajectory.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
 
-
-
+#include "rclcpp/rclcpp.hpp"
 #include <ament_index_cpp/get_package_share_directory.hpp>
 
-class SobitProMain : public rclcpp::Node {
-    public:
-        SobitProMain();
+
+namespace sobit_pro
+{
+
+class SobitProMain : public rclcpp::Node
+{
+public:
+  explicit SobitProMain(const rclcpp::NodeOptions & options);
+  ~SobitProMain();
+  
+  bool start_up_sound();
+  bool shut_down_sound();
+  void control_wheel();
     
-        bool start_up_sound();
-        bool shut_down_sound();
-        void control_wheel();
+private:    
+  // ROS2 I/F
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr    sub_vel_;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr sub_joint_info_;
+
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr               pub_odometry_;
+  rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr pub_steer_joint_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr      pub_wheel_joint_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr                   pub_wheels_error_;
+
+  // コールバック
+  void callback(const geometry_msgs::msg::Twist::SharedPtr msg);
+  void joint_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
+
+  // ユーティリティ関数
+  double getJointPos(const std::string& joint_name);
+  double getJointVel(const std::string& joint_name);
+  void setPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt);
+  void addPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt);
+  void checkPublishersConnection(rclcpp::PublisherBase::SharedPtr pub);
+
+  // ロボット状態管理
+  int32_t wheel_fl_init_pos, wheel_fr_init_pos, wheel_bl_init_pos, wheel_br_init_pos;
+  int32_t wheel_fl_curt_pos, wheel_fr_curt_pos, wheel_bl_curt_pos, wheel_br_curt_pos;
+  int32_t steer_fl_curt_pos, steer_fr_curt_pos, steer_bl_curt_pos, steer_br_curt_pos;
+
+  int32_t* set_steer_pos;
+  int32_t* set_wheel_vel;
+
+  int32_t motion;
+  int32_t prev_motion = -1;
+
+  std::map<std::string, double> joints_pos;
+  std::map<std::string, double> joints_vel;
+
+  nav_msgs::msg::Odometry result_odom;
+  nav_msgs::msg::Odometry prev_odom;
+
+  std_msgs::msg::Bool wheels_error;
+
+};
     
-    private:
-        int motion;
-        // ROS2 I/F
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_vel_;
-        rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr sub_joint_info_;
-        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odometry_;
-        rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr pub_steer_joint_;
-        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_wheel_joint_;
-        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub_wheels_error_;
-    
-        // コールバック
-        void callback(const geometry_msgs::msg::Twist::SharedPtr msg);
-        void joint_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
-    
-        // ユーティリティ関数
-        double getJointPos(const std::string& joint_name);
-        double getJointVel(const std::string& joint_name);
-        void setPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt);
-        void addPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt);
-        void checkPublishersConnection(rclcpp::PublisherBase::SharedPtr pub);
-    
-        // ロボット状態管理
-        int32_t wheel_fl_init_pos, wheel_fr_init_pos, wheel_bl_init_pos, wheel_br_init_pos;
-        int32_t wheel_fl_curt_pos, wheel_fr_curt_pos, wheel_bl_curt_pos, wheel_br_curt_pos;
-        int32_t steer_fl_curt_pos, steer_fr_curt_pos, steer_bl_curt_pos, steer_br_curt_pos;
-    
-        int32_t* set_steer_pos;
-        int32_t* set_wheel_vel;
-        int32_t prev_motion = -1;
-    
-        std::map<std::string, double> joints_pos;
-        std::map<std::string, double> joints_vel;
-    
-        nav_msgs::msg::Odometry result_odom;
-        nav_msgs::msg::Odometry prev_odom;
-        std_msgs::msg::Bool wheels_error;
-    };
-    
-    // 実装部
-    inline SobitProMain::SobitProMain() : Node("sobit_pro_main")
-    {
-        sub_vel_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "mobile_base/commands/velocity", 10, std::bind(&SobitProMain::callback, this, std::placeholders::_1));
-    
-        sub_joint_info_ = this->create_subscription<sensor_msgs::msg::JointState>(
-            "joint_states", 10, std::bind(&SobitProMain::joint_callback, this, std::placeholders::_1));
-    
-        pub_odometry_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
-        pub_steer_joint_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("steer_trajectory_controller/command", 10);
-        pub_wheel_joint_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("wheel_trajectory_controller/command", 10);
-        pub_wheels_error_ = this->create_publisher<std_msgs::msg::Bool>("wheels_error", 10);
-    }
-    
-    inline double SobitProMain::getJointPos(const std::string& joint_name) {
-        return joints_pos[joint_name];
-    }
-    
-    inline double SobitProMain::getJointVel(const std::string& joint_name) {
-        return joints_vel[joint_name];
-    }
-    
-    inline void SobitProMain::setPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt) {
-        jt->joint_names = {joint_name};
-        trajectory_msgs::msg::JointTrajectoryPoint point;
-        point.positions = {rad};
-        point.time_from_start = rclcpp::Duration::from_seconds(sec);
-        jt->points = {point};
-    }
-    
-    inline void SobitProMain::addPosJointTrajectory(const std::string& joint_name, double rad, double sec, trajectory_msgs::msg::JointTrajectory* jt) {
-        jt->joint_names.push_back(joint_name);
-        if (jt->points.empty()) {
-            jt->points.emplace_back();
-            jt->points[0].time_from_start = rclcpp::Duration::from_seconds(sec);
-        }
-        jt->points[0].positions.push_back(rad);
-        jt->points[0].time_from_start = rclcpp::Duration::from_seconds(sec);
-    }
-    
-    inline void SobitProMain::checkPublishersConnection(rclcpp::PublisherBase::SharedPtr pub) {
-        rclcpp::Rate rate(10);
-        while (pub->get_subscription_count() == 0 && rclcpp::ok()) {
-            rate.sleep();
-        }
-    }
-    
-    // コールバック関数は別途実装
-    
-    #endif // SOBIT_PRO_MAIN_HPP_
+// 実装部
+inline double SobitProMain::getJointPos(const std::string& joint_name)
+{
+  return joints_pos[joint_name];
+}
+
+inline double SobitProMain::getJointVel(const std::string& joint_name)
+{
+  return joints_vel[joint_name];
+}
+
+inline void SobitProMain::setPosJointTrajectory(
+  const std::string& joint_name,
+  double rad, double sec,
+  trajectory_msgs::msg::JointTrajectory* jt)
+{
+  trajectory_msgs::msg::JointTrajectory      joint_trajectory;
+  trajectory_msgs::msg::JointTrajectoryPoint joint_trajectory_point;
+
+  joint_trajectory.joint_names.push_back( joint_name );
+  joint_trajectory_point.positions.push_back( rad );
+  // joint_trajectory_point.velocities.push_back( 0.0 );
+  // joint_trajectory_point.accelerations.push_back( 0.0 );
+  // joint_trajectory_point.effort.push_back( 0.0 );
+  joint_trajectory_point.time_from_start = rclcpp::Duration::from_seconds(sec);
+  joint_trajectory.points.push_back( joint_trajectory_point );
+
+  *jt = joint_trajectory;
+}
+
+inline void SobitProMain::addPosJointTrajectory(
+  const std::string& joint_name,
+  double rad, double sec,
+  trajectory_msgs::msg::JointTrajectory* jt)
+{
+  trajectory_msgs::msg::JointTrajectory joint_trajectory = *jt;
+
+  joint_trajectory.joint_names.push_back( joint_name );
+  joint_trajectory.points[0].positions.push_back( rad );
+  // joint_trajectory.points[0].velocities.push_back( 0.0 );
+  // joint_trajectory.points[0].accelerations.push_back( 0.0 );
+  // joint_trajectory.points[0].effort.push_back( 0.0 );
+  joint_trajectory.points[0].time_from_start = rclcpp::Duration::from_seconds(sec);
+
+  *jt = joint_trajectory;
+}
+
+inline void SobitProMain::checkPublishersConnection(rclcpp::PublisherBase::SharedPtr pub) {
+  rclcpp::Rate rate(10);
+  while (pub->get_subscription_count() == 0 && rclcpp::ok()) {
+    rate.sleep();
+  }
+}
+
+} // namespace sobit_pro
+
+#endif // SOBIT_PRO_MAIN_HPP_
