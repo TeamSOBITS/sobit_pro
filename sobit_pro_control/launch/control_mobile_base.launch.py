d2233ad2f3a64ce3c42bbaa588d1b52b6398059d
MrKeih99 <kvalentincardenas@hotmail.com>
2025-04-12 21:42:13 +0900

    [UPD] add opaque function

--- sobit_pro_control/launch/control_mobile_base.launch.py
+++ sobit_pro_control/launch/control_mobile_base.launch.py
@@ -1,17 +1,30 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    robot_name = "sobit_pro"
-    robot_id = 0
-    enable_gz = 'True'
+    arg_robot_name = DeclareLaunchArgument('robot_name', default_value='sobit_pro')
+    arg_enable_gz  = DeclareLaunchArgument('enable_gz', default_value='False')
+
+    return LaunchDescription([
+        arg_robot_name,
+        arg_enable_gz,
+        OpaqueFunction(function = launch_gz),
+    ])
+
+
+def launch_gz(context, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot_name').perform(context)
+    enable_gz  = LaunchConfiguration('enable_gz').perform(context)
     
     move_base_node = Node(
         package="sobit_pro_control",
         executable="sobit_pro_control_node",
         name="sobit_pro_control",
-        namespace=robot_name if robot_id == 0 else f"{robot_name}_{robot_id}",
+        namespace=robot_name,
         parameters=[
             {"use_sim_time": True if enable_gz == 'True' else False},
         ],
