d1d69b3296b39e802c57d8506f4d560c1f0978c6
Lucasmotabr <lucasbrunopoli@hotmail.com>
2025-08-03 07:29:41 +0900

    [UPD] thresholdchange

--- sobit_pro_control/src/sobit_pro_main.cpp
+++ sobit_pro_control/src/sobit_pro_main.cpp
@@ -233,7 +233,7 @@ bool SobitProMain::shut_down_sound()
 void SobitProMain::control_callback()
 {
   static std::array<double, 4> last_sent_steer_pos = {0.0, 0.0, 0.0, 0.0};
-  constexpr double STEER_PUBLISH_EPSILON = 0.09; // Minimum change to consider as new command [rad]
+  constexpr double STEER_PUBLISH_EPSILON = 0.05; // Minimum change to consider as new command [rad]
 
   // Wait for joint states
   if (joints_pos.empty() || joints_vel.empty()) {
